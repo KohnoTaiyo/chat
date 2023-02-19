ab0239ad94934c94412106e24ac7723708258746
KohnoTaiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-02-19 14:42:25 +0900

    setting css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
-import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import "../styles/reset.css"
+import "../styles/globals.scss"
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Component {...pageProps} />
-  </>
-)
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
 export default MyApp
--- src/Templates/Layout.tsx
+++ src/Templates/Layout.tsx
@@ -1,10 +1,21 @@
-import { FC } from "react"
+import Head from "next/head"
+import { FC, ReactNode } from "react"
 
 type LayoutProps = {
   title?: string
   description?: string
+  children: ReactNode
 }
 
-const Layout: FC<LayoutProps> = (props) => <></>
+const Layout: FC<LayoutProps> = (props) => (
+  <>
+    <Head>
+      <title>{props.title || "チャットWebアプリ"}</title>
+      <meta name="description" content={props.description || "課題のチャットWebアプリです"} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    {props.children}
+  </>
+)
 
 export { Layout }
